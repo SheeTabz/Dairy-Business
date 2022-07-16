369e546ac7b1572a1f5e77b1d24bbc840d8d71b1
SheeTabz <wmuriithitabitha@gmail.com>
2022-07-16 14:31:16 +0300

    Addng functionality to the total button

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,85 @@ const shedA = document.getElementById('sheda')
 const shedB= document.getElementById('shedb')
 const shedC = document.getElementById('shedc')
 const shedD = document.getElementById('shedd')
+const butn = document.getElementsByClassName('total')
+const forms = document.getElementById('forms')
+const display = document.getElementsByClassName('inner')
 
 
+//functions
 
-const monitor = [{ name: "A", litre: 510 },
-{ name: "B", litre: 308 },
-{ name: "C", litre: 486 },
-{ name: "D", litre: 572 },
-];
+forms.addEventListener('submit', (e) => {
+   e.preventDefault(); // preventDefault form from submitting
+   
+// Objects for the inputs
+let inputs = [{name: 'A', litres: shedA.value},
+       {name: 'B', litres: shedB.value }, {name: 'C', litres:shedC.value},
+       {name: 'D', litres: shedD.value},]
+//function that returns the value of the specified in the input field       
+const totalInput = function (){   
+// looping through the input array and giving the output value
+inputs.forEach(data=>{ 
+    const li=document.createElement('li');
+    li.innerText=`You production in ${data.name} is ${data.litres} litres`
+    ul.appendChild(li);
 
-const totalProduction = function () {
-    for (let monitoring of monitor) {
-    return `Your production in ${monitoring.name} is ${monitoring.litre} litres per day.`
-}  
+ });
 }
+totalInput()
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const monitor = [{ name: "A", litre: 510 },
+// { name: "B", litre: 308 },
+// { name: "C", litre: 486 },
+// { name: "D", litre: 572 },
+// ];
+// const shades=
+//     {
+//         shade1:shedA,
+//         shade2:shedB,
+//         shade3:shedC,
+//         shade4:shedD,
+//     }
+
+
+// const totalProduction = function () {
+// //     for (let monitoring of monitor) {
+// //     return `Your production in ${monitoring.name} is ${monitoring.litre} litres per day.`
+// // }  
+// for (const item in shades) {
+//     return `Your production in ${item} is ${item}`
+// }
+
+// }
